a3969b465d7d7e211cd9cf3e9f05ab668651543f
Dmitriy Komkov <d.komkov@web-izmerenie.ru>
2014-06-09 12:24:39 +0400

    replaced homestead component to pure bitrix API

--- bitrix/templates/.default/components/bitrix/news/homestead/bitrix/news.list/.default/template.php
+++
@@ -1,9 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
-        <img alt="<?=$arItem["NAME"]?>" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" />
-        <span><span><?=$arItem["NAME"]?></span></span>
-    </a>
-<?endforeach;?>
-
