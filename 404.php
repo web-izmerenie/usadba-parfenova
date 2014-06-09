6ce1ec95db3cfc147022cc41428368c076d6f1b6
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-06-09 12:25:36 +0400

    404 page plug

---
+++ 404.php
@@ -0,0 +1,5 @@
+<?
+define("error_404", "Y");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Усадьба Парфёнова");
+?>404 Страница не найдена<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
