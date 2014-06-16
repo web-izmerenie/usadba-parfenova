dc8e199b04b8b73e1d0b1894b38b7743ea835640
Dmitriy Komkov <d.komkov@web-izmerenie.ru>
2014-06-16 11:40:19 +0400

    new localization vars

--- bitrix/templates/main/lang/en/header.php
+++ bitrix/templates/main/lang/en/header.php
@@ -1,4 +1,13 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $MESS["TMPL_CREATED_BY"] = 'Created by <a href="http://web-izmerenie.ru" class="logo" title="Web izmerenie">Web izmerenie</a>';
+$MESS["HOMESTEAD"] = 'Homestead';
+$MESS["RUB_DAY"] = '<span>rub.</span>
+                    <span>/</span>
+                    <span>day</span>';
+$MESS["TRANSFER"] = "Transfer";
+$MESS["TRANSFER_LONG"] = "Manor offers a shuttle service from Krasnodar to guest house";
+$MESS["SHOW_MORE"] = "Show more";
+$MESS["ACTIVITIES"] = "Activities";
+$MESS["MAKE_REVIEW"] = "Make review";
 ?>
