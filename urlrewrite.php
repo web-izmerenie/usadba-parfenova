982c83fbaaadb9fe80ca707a509af20bb2a2406f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-06-16 16:59:38 +0400

    urlrewrite.php by bitrix

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,22 +1,23 @@
-<?php
-$arUrlRewrite=array (
-    array(
-        "CONDITION" => "#^/homestead/([a-z]+)/([a-z]+)/(.*)#",
-        "RULE" => "ELEMENT_CODE=$2",
-        "ID" => "bitrix:news.detail",
-        "PATH" => "/homestead/detail.php",
-    ),
-    array(
-        "CONDITION" => "#^/activities/([a-z]+)/details/#",
-        "RULE" => "ELEMENT_CODE=$1",
-        "ID" => "",
-        "PATH" => "/activities/detail.php",
-    ),
-    array(
-        "CONDITION" => "#^/activities/([a-z]+)/#",
-        "RULE" => "ELEMENT_CODE=$1",
-        "ID" => "",
-        "PATH" => "/activities/title.php",
-    ),
-    
+<?
+$arUrlRewrite = array(
+	array(
+		"CONDITION" => "#^/homestead/([a-z]+)/([a-z]+)/(.*)#",
+		"RULE" => "ELEMENT_CODE=\$2",
+		"ID" => "bitrix:news.detail",
+		"PATH" => "/homestead/detail.php",
+	),
+	array(
+		"CONDITION" => "#^/activities/([a-z]+)/details/#",
+		"RULE" => "ELEMENT_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/activities/detail.php",
+	),
+	array(
+		"CONDITION" => "#^/activities/([a-z]+)/#",
+		"RULE" => "ELEMENT_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/activities/title.php",
+	),
 );
+
+?>
