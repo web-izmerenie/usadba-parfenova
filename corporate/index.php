fac36c8cf5bf089d6a31e35c8cb18f00e23f3c9d
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-06-16 16:30:20 +0400

    "corporate" section

--- corporate/index.php
+++ corporate/index.php
@@ -1,8 +1,18 @@
 <?
+define("PAGE_TITLE", "Y");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Корпоративным клиентам");
 ?>
 
-Text here....
+<section class="corporate">
+    <div class="big_section_photo"><img alt="" src="/upload/tmp/corporate_big_photo.jpg" /></div>
+    <div class="notation_block">
+        <p>Интерьер номеров продуман до мельчайших деталей. Каждый из номеров
+        оснащён современным LCD-телевизором и DVD, спутниковой антенной,
+        холодильником с прохладительными напитками, кондиционером. В ванных
+        комнатах сантехника и душевые кабины европейского качества, фен,
+        косметические средства для личной гигиены, махровые халаты и тапочки.</p>
+    </div>
+</section>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
