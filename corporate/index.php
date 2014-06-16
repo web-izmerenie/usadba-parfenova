1e3621ef02be52fa1436d1f904178329d566adf7
Dmitriy Komkov <d.komkov@web-izmerenie.ru>
2014-06-16 18:09:25 +0400

    "corporate" section

--- corporate/index.php
+++ corporate/index.php
@@ -3,16 +3,13 @@ define("PAGE_TITLE", "Y");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Корпоративным клиентам");
 ?>
-
-<section class="corporate">
-    <div class="big_section_photo"><img alt="" src="/upload/tmp/corporate_big_photo.jpg" /></div>
-    <div class="notation_block">
-        <p>Интерьер номеров продуман до мельчайших деталей. Каждый из номеров
-        оснащён современным LCD-телевизором и DVD, спутниковой антенной,
-        холодильником с прохладительными напитками, кондиционером. В ванных
-        комнатах сантехника и душевые кабины европейского качества, фен,
-        косметические средства для личной гигиены, махровые халаты и тапочки.</p>
-    </div>
-</section>
-
+<?
+    $APPLICATION->IncludeFile(
+        "inc/tmpl/corporate.php",
+        array(),
+        array(
+            "SHOW_BORDER" => false
+        )
+    );
+?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
