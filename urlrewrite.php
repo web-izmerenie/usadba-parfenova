358d407e039e4c969c8a2269b9fa0331a709c476
Dmitriy Komkov <d.komkov@web-izmerenie.ru>
2014-06-16 11:36:01 +0400

    urlrewrite.php for "activities"

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,11 +1,22 @@
-<?
-$arUrlRewrite = array(
-	array(
-		"CONDITION" => "#^/homestead/([a-zA-Z]+)/([a-zA-Z]+)/#",
-		"RULE" => "ELEMENT_CODE=\$2",
-		"ID" => "bitrix:news.detail",
-		"PATH" => "/homestead/detail.php",
-	),
+<?php
+$arUrlRewrite=array (
+    array(
+        "CONDITION" => "#^/homestead/([a-z]+)/([a-z]+)/(.*)#",
+        "RULE" => "ELEMENT_CODE=$2",
+        "ID" => "bitrix:news.detail",
+        "PATH" => "/homestead/detail.php",
+    ),
+    array(
+        "CONDITION" => "#^/activities/([a-z]+)/details/#",
+        "RULE" => "ELEMENT_CODE=$1",
+        "ID" => "",
+        "PATH" => "/activities/detail.php",
+    ),
+    array(
+        "CONDITION" => "#^/activities/([a-z]+)/#",
+        "RULE" => "ELEMENT_CODE=$1",
+        "ID" => "",
+        "PATH" => "/activities/title.php",
+    ),
+    
 );
-
-?>
