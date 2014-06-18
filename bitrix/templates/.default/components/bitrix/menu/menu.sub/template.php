2a29f16b2f4464e648e06d8fd7140b5a0d3632f0
Dmitriy Komkov <d.komkov@web-izmerenie.ru>
2014-06-18 15:29:39 +0400

    target values for menu

--- bitrix/templates/.default/components/bitrix/menu/menu.sub/template.php
+++ bitrix/templates/.default/components/bitrix/menu/menu.sub/template.php
@@ -6,6 +6,9 @@
 foreach($arResult as $arItem):
 	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
 		continue;
+    
+    $target = stristr($arItem["LINK"], "price") ? "_blank" : "_self";
+        
 ?>
 	<?if($arItem["SELECTED"]):?>
 		<span><?=$arItem["TEXT"]?></span>
@@ -13,7 +16,7 @@ foreach($arResult as $arItem):
         <?if(stristr($arItem["LINK"], "ask")):?>
             <a class="ask_a_question"><?=$arItem["TEXT"]?></a>
         <?else:?>
-            <a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+            <a target="<?=$target?>" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
         <?endif;?>
 	<?endif?>
 	
