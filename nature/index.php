cc0342b317c1a56be23014fcbad706ec8c076d03
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-07 02:50:55 +0400

    "nature": filled by content

--- nature/index.php
+++ nature/index.php
@@ -9,12 +9,12 @@ $APPLICATION->SetTitle("Дары природы");
 	<ul class="list">
 		<li>
 			<dl>
-				<dt><img alt="Мёд майский" src="/upload/tmp/nature_1.png" /></dt>
+				<dt><img alt="Мёд майский" src="/upload/tmp/nature/01.png" /></dt>
 				<dd>
 					<div class="nature_detail">
 						<div class="center_wrap_1">
 						<div class="center_wrap_2">
-							<img alt="Мёд майский" src="/upload/tmp/nature_detail.png" class="detail_pic" />
+							<img alt="Мёд майский" src="/upload/tmp/nature/detail/01.png" class="detail_pic" />
 							<div class="detail_text">
 								<h3>Мёд майский</h3>
 								<div class="text">
@@ -34,33 +34,20 @@ $APPLICATION->SetTitle("Дары природы");
 		</li>
 		<li>
 			<dl>
-				<dt><img alt="Мёд" src="/upload/tmp/nature_2.png" /></dt>
+				<dt><img alt="Мёд каштановый" src="/upload/tmp/nature/02.png" /></dt>
 				<dd>
 					<div class="nature_detail">
 						<div class="center_wrap_1">
 						<div class="center_wrap_2">
-							<img alt="Мёд майский" src="/upload/tmp/nature_detail.png" class="detail_pic" />
+							<img alt="Мёд каштановый" src="/upload/tmp/nature/detail/02.png" class="detail_pic" />
 							<div class="detail_text">
-								<h3>Мёд майский</h3>
+								<h3>Мёд каштановый</h3>
 								<div class="text">
-									<p>Содержит огромное количество витаминов и&nbsp;микроэлементов.
-									Рекомендуется для&nbsp;повышения иммунитета, восстановления
-									защитных сил&nbsp;организма. Помогает при&nbsp;стрессах
-									и&nbsp;перенапряжениях, снимает головные боли. Усиливает
-									физическую и&nbsp;умственную активность, укрепляет сердце,
-									благотворно действует на&nbsp;печень.</p>
-									<p>Содержит огромное количество витаминов и&nbsp;микроэлементов.
-									Рекомендуется для&nbsp;повышения иммунитета, восстановления
-									защитных сил&nbsp;организма. Помогает при&nbsp;стрессах
-									и&nbsp;перенапряжениях, снимает головные боли. Усиливает
-									физическую и&nbsp;умственную активность, укрепляет сердце,
-									благотворно действует на&nbsp;печень.</p>
-									<p>Содержит огромное количество витаминов и&nbsp;микроэлементов.
-									Рекомендуется для&nbsp;повышения иммунитета, восстановления
-									защитных сил&nbsp;организма. Помогает при&nbsp;стрессах
-									и&nbsp;перенапряжениях, снимает головные боли. Усиливает
-									физическую и&nbsp;умственную активность, укрепляет сердце,
-									благотворно действует на&nbsp;печень.</p>
+									<p>Содержит много минеральных солей и&nbsp;аминокислот.
+									Рекомендуется при&nbsp;тромбофлебите, варикозном расширении&nbsp;вен,
+									простате, простудных заболеваниях, бронхите, астме. Оказывает благотворное
+									действие на&nbsp;сосудистую систему, нормализует давление, отлично
+									укрепляет иммунитет.</p>
 								</div>
 							</div>
 						</div><!--.center_wrap_2-->
@@ -71,26 +58,98 @@ $APPLICATION->SetTitle("Дары природы");
 		</li>
 		<li>
 			<dl>
-				<dt><img alt=".." src="/upload/tmp/nature_3.png" /></dt>
-				<dd></dd>
+				<dt><img alt="Варенье абрикосовое" src="/upload/tmp/nature/03.png" /></dt>
+				<dd>
+					<div class="nature_detail">
+						<div class="center_wrap_1">
+						<div class="center_wrap_2">
+							<img alt="Варенье абрикосовое" src="/upload/tmp/nature/detail/03.png" class="detail_pic" />
+							<div class="detail_text">
+								<h3>Варенье абрикосовое</h3>
+								<div class="text">
+									<p>Прекрасный десерт к&nbsp;любому столу. Содержание витамина&nbsp;А
+									и&nbsp;калия больше, чем&nbsp;в&nbsp;других фруктах. Варенье
+									из&nbsp;абрикосов в&nbsp;умеренных количествах будет полезно беременным,
+									детям и&nbsp;пожилым людям, а&nbsp;также страдающим сердечно&nbsp;—
+									сосудистыми заболеваниями.</p>
+								</div>
+							</div>
+						</div><!--.center_wrap_2-->
+						</div><!--.center_wrap_1-->
+					</div><!--.nature_detail-->
+				</dd>
 			</dl>
 		</li>
 		<li>
 			<dl>
-				<dt><img alt=".." src="/upload/tmp/nature_4.png" /></dt>
-				<dd></dd>
+				<dt><img alt="Варенье малиновое" src="/upload/tmp/nature/04.png" /></dt>
+				<dd>
+					<div class="nature_detail">
+						<div class="center_wrap_1">
+						<div class="center_wrap_2">
+							<img alt="Варенье малиновое" src="/upload/tmp/nature/detail/04.png" class="detail_pic" />
+							<div class="detail_text">
+								<h3>Варенье малиновое</h3>
+								<div class="text">
+									<p>Малиновое варенье содержит натуральную салициловую кислоту&nbsp;—
+									составляющее большинства противовоспалительных и&nbsp;жаропонижающих
+									средств. Витамины&nbsp;А,&nbsp;Е,&nbsp;РР,&nbsp;В2
+									отвечают&nbsp;за&nbsp;ровный цвет&nbsp;лица. Варенье способствует
+									улучшению кроветворения.</p>
+								</div>
+							</div>
+						</div><!--.center_wrap_2-->
+						</div><!--.center_wrap_1-->
+					</div><!--.nature_detail-->
+				</dd>
 			</dl>
 		</li>
 		<li>
 			<dl>
-				<dt><img alt=".." src="/upload/tmp/nature_5.png" /></dt>
-				<dd></dd>
+				<dt><img alt="Черника с сахаром" src="/upload/tmp/nature/05.png" /></dt>
+				<dd>
+					<div class="nature_detail">
+						<div class="center_wrap_1">
+						<div class="center_wrap_2">
+							<img alt="Черника с сахаром" src="/upload/tmp/nature/detail/05.png" class="detail_pic" />
+							<div class="detail_text">
+								<h3>Черника с сахаром</h3>
+								<div class="text">
+									<p>Черника значительно улучшает зрение человека, ускоряет обновление
+									сетчатой оболочки глаза. Оптимальное соотношение витаминов&nbsp;С,
+									группы&nbsp;В, <span class="non_break">Р-витаминоактивных</span>
+									дубильных веществ, магния, железа, меди обуславливают сосудоукрепляющее,
+									противобактериальное свойства черники.</p>
+								</div>
+							</div>
+						</div><!--.center_wrap_2-->
+						</div><!--.center_wrap_1-->
+					</div><!--.nature_detail-->
+				</dd>
 			</dl>
 		</li>
 		<li>
 			<dl>
-				<dt><img alt=".." src="/upload/tmp/nature_6.png" /></dt>
-				<dd></dd>
+				<dt><img alt="Опята маринованные" src="/upload/tmp/nature/06.png" /></dt>
+				<dd>
+					<div class="nature_detail">
+						<div class="center_wrap_1">
+						<div class="center_wrap_2">
+							<img alt="Опята маринованные" src="/upload/tmp/nature/detail/06.png" class="detail_pic" />
+							<div class="detail_text">
+								<h3>Опята маринованные</h3>
+								<div class="text">
+									<p>Прекрасная закуска к&nbsp;любому столу. Опята благотворно влияют
+									на&nbsp;функционирование щитовидной железы. Также в&nbsp;опятах
+									содержится масса противораковых веществ. Содержат
+									витамины&nbsp;В2,&nbsp;С,&nbsp;Е,&nbsp;РР, магний, натрий, калий,
+									фосфор, кальций и&nbsp;железо.</p>
+								</div>
+							</div>
+						</div><!--.center_wrap_2-->
+						</div><!--.center_wrap_1-->
+					</div><!--.nature_detail-->
+				</dd>
 			</dl>
 		</li>
 	</ul>
