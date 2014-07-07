132b1890075265eb1d239591df6c5152adc97788
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-07-07 06:01:51 +0400

    target=_blank for WI logo in footer

--- bitrix/templates/main/lang/ru/header.php
+++ bitrix/templates/main/lang/ru/header.php
@@ -1,6 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
-$MESS["TMPL_CREATED_BY"] = 'Сделано в <a href="http://web-izmerenie.ru" class="logo" title="Web izmerenie">Web izmerenie</a>';
+$MESS["TMPL_CREATED_BY"] = 'Сделано в <a href="http://web-izmerenie.ru" class="logo" title="Web izmerenie" target="_blank">Web izmerenie</a>';
 $MESS["HOMESTEAD"] = 'Усадьба';
 $MESS["RUB_DAY"] = '<span>руб.</span>
                     <span>/</span>
