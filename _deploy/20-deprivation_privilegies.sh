6e1e1b5683b86227e1ace3d84d9b82840116c2db
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-13 06:50:26 +0400

    added shebang for "deprivation_privilegies" deploy task

--- _deploy/20-deprivation_privilegies.sh
+++ _deploy/20-deprivation_privilegies.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")" WD="$(dirname "$0")/../" ../deploy.sh; exit "$?"; fi
 
 info_inline "Deprivation of privileges group and others"
