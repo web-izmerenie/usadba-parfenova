4ad14f9e452f7811a8be249e33584a792392bbb4
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-14 05:23:38 +0400

    new photos html structure of "homestead" detail page template

--- bitrix/templates/.default/components/bitrix/news.detail/guesthouse.detail/template.php
+++ bitrix/templates/.default/components/bitrix/news.detail/guesthouse.detail/template.php
@@ -95,13 +95,19 @@
 			</div>
 		<?}?>
 		<?if($arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"]){?>
-			<ul class="photos">
-				<?foreach($arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"] as $image){?>
-					<?$thumb = CFile::ResizeImageGet($image, array("width" => "295", "height" => "195"), BX_RESIZE_IMAGE_EXACT);?>
-					<?$origin = CFile::ResizeImageGet($image, array("width" => "1200", "height" => "1200"), BX_RESIZE_IMAGE_PROPORTIONAL);?>
-					<li><a href="<?=$origin["src"]?>" target="_blank"><img alt="" src="<?=$thumb["src"]?>" /></a></li>
-				<?}?>
-			</ul>
+			<div class="photogallery">
+				<div class="new_gallery_big">
+					<a class="prev"></a>
+					<a class="next"></a>
+				</div>
+				<ul class="new_gallery_list">
+					<?foreach($arResult["DISPLAY_PROPERTIES"]["GALLERY"]["VALUE"] as $image){?>
+						<?$thumb = CFile::ResizeImageGet($image, array("width" => "57", "height" => "57"), BX_RESIZE_IMAGE_EXACT);?>
+						<?$origin = CFile::ResizeImageGet($image, array("width" => "940", "height" => "454"), BX_RESIZE_IMAGE_EXACT);?>
+						<li><a href="<?=$origin["src"]?>" target="_blank"><img alt="" src="<?=$thumb["src"]?>" /></a></li>
+					<?}?>
+				</ul>
+			</div>
 		<?}?>
 		<div class="prices">
 			<?foreach($arResult["DISPLAY_PROPERTIES"]["PRICE"]["VALUE"] as $key => $price){?>
