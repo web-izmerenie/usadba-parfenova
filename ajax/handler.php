73b50dc21db6774588092686d37cedbdf7caeaa3
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-17 11:45:26 +0400

    fixed add a question form handler in ajax/handler.php

--- ajax/handler.php
+++ ajax/handler.php
@@ -7,6 +7,7 @@ $response["status"] = "error";
 
 if (!$_POST["action"]) {
 	$response["status"] = "error";
+	$response["error_code"] = "no_action_field";
 } else {
 	if ($_POST["action"] == "add_review") { // ADD UNACTIVE PREMODERATED REVIEW ITEM
 		if (!$_POST["name"] || !$_POST["review_text"]) {
@@ -61,9 +62,7 @@ if (!$_POST["action"]) {
 				}
 			}
 		}
-	}
-
-	if ($_POST["action"] == "add_question") { // ADD QUESTION FROM USER ASKED IN PUBLIC SITE PATH
+	} elseif ($_POST["action"] == "add_question") { // ADD QUESTION FROM USER ASKED IN PUBLIC SITE PATH
 		if (!$_POST["name"] || !$_POST["question_text"] || !$_POST["phone"]) {
 			$response["status"] = "error";
 			$response["error_code"] = "required_fields";
@@ -75,60 +74,60 @@ if (!$_POST["action"]) {
 			if (!$_POST["phone"])
 				$response["fields_names"][] = "phone";
 		} else {
-			if ($_POST["email"]) {
-				if (!preg_match("/^(.+)\@(.+)$/", $_POST["email"])) {
+			if ($_POST["email"] && !preg_match("/^(.+)\@(.+)$/", $_POST["email"])) {
+				$response["status"] = "error";
+				$response["error_code"] = "incorrect_fields";
+				$response["fields_names"][] = "email";
+			} else {
+				if ($_POST["phone"] && !preg_match("/([0-9]+)/", $_POST["phone"])) {
 					$response["status"] = "error";
 					$response["error_code"] = "incorrect_fields";
-					$response["fields_names"][] = "email";
+					$response["fields_names"][] = "phone";
 				} else {
-					if ($_POST["phone"] && !preg_match("/([0-9]+)/", $_POST["phone"])) {
-						$response["status"] = "error";
-						$response["error_code"] = "incorrect_fields";
-						$response["fields_names"][] = "phone";
-					} else {
-						$replace = preg_split("/[\n\r]+/", $_POST["question_text"]);
-						$question_text = "";
-						foreach ($replace as $string) {
-							$string = trim($string);
-							if ($string)
-								$question_text .= "<p>".$string."</p>";
-						}
+					$replace = preg_split("/[\n\r]+/", $_POST["question_text"]);
+					$question_text = "";
+					foreach ($replace as $string) {
+						$string = trim($string);
+						if ($string)
+							$question_text .= "<p>".$string."</p>";
+					}
 
-						CModule::IncludeModule("iblock");
-						$el = new CIBlockElement;
-						$arFields = array(
-							"IBLOCK_ID" => "6", //questions
-							"ACTIVE" => "N",
-							"DATE_ACTIVE_FROM" => date("d.m.Y H:i:s"),
-							"NAME" => $_POST["name"],
-							"PREVIEW_TEXT" => $question_text,
-							"PREVIEW_TEXT_TYPE" => "html",
-							"PROPERTY_VALUES" => array(
-								"EMAIL" => $_POST["email"],
-								"PHONE" => $_POST["phone"],
-								"LANG" => $_POST["lang"]
-							)
+					CModule::IncludeModule("iblock");
+					$el = new CIBlockElement;
+					$arFields = array(
+						"IBLOCK_ID" => "6", //questions
+						"ACTIVE" => "N",
+						"DATE_ACTIVE_FROM" => date("d.m.Y H:i:s"),
+						"NAME" => $_POST["name"],
+						"PREVIEW_TEXT" => $question_text,
+						"PREVIEW_TEXT_TYPE" => "html",
+						"PROPERTY_VALUES" => array(
+							"EMAIL" => $_POST["email"],
+							"PHONE" => $_POST["phone"],
+							"LANG" => $_POST["lang"]
+						)
+					);
+
+					$add = $el->Add($arFields);
+
+					if ($add) {
+						$response["status"] = "success";
+
+						$site_id = $_POST["lang"] == "ru" ? "s1" : "en";
+						$send = CEvent::SendImmediate(
+							"QUESTION_SENT",
+							$site_id,
+							false,
+							"N"
 						);
-
-						$add = $el->Add($arFields);
-
-						if ($add) {
-							$response["status"] = "success";
-
-							$site_id = $_POST["lang"] == "ru" ? "s1" : "en";
-							$send = CEvent::SendImmediate(
-								"QUESTION_SENT",
-								$site_id,
-								false,
-								"N"
-							);
-						} else {
-							$response["status"] = "error";
-						}
+					} else {
+						$response["status"] = "error";
 					}
 				}
 			}
 		}
+	} else {
+		$response["error_code"] = 'unknown_action';
 	}
 }
 
