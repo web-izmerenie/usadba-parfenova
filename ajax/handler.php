b17e00e5f850f1119a3a4a3b98f0c286b2e68582
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-18 09:54:09 +0400

    ajax handler for "reserve" form (validate and save to iblock)

--- ajax/handler.php
+++ ajax/handler.php
@@ -59,6 +59,7 @@ if (!$_POST["action"]) {
 					);
 				} else {
 					$response["status"] = "error";
+					$response["error_code"] = "add_to_iblock";
 				}
 			}
 		}
@@ -122,7 +123,77 @@ if (!$_POST["action"]) {
 						);
 					} else {
 						$response["status"] = "error";
+						$response["error_code"] = "add_to_iblock";
 					}
+				}
+			}
+		}
+	} elseif ($_POST["action"] == "reserve") {
+		if (
+			!$_POST["house_id"] || !$_POST["house_name"] ||
+			!$_POST["people_count"] || !$_POST["hidden_people_count"] ||
+			!$_POST["in_date"] || !$_POST["out_date"] ||
+			!$_POST["name"] || !$_POST["last_name"] || !$_POST["phone"]
+		) {
+			$response["status"] = "error";
+			$response["error_code"] = "required_fields";
+
+			if (!$_POST["house_id"] || !$_POST["house_name"])
+				$response["fields_names"][] = "house_name";
+			if (!$_POST["people_count"] || !$_POST["hidden_people_count"])
+				$response["fields_names"][] = "people_count";
+			if (!$_POST["in_date"])
+				$response["fields_names"][] = "in_date";
+			if (!$_POST["out_date"])
+				$response["fields_names"][] = "out_date";
+			if (!$_POST["name"])
+				$response["fields_names"][] = "name";
+			if (!$_POST["last_name"])
+				$response["fields_names"][] = "last_name";
+			if (!$_POST["phone"])
+				$response["fields_names"][] = "phone";
+		} else {
+			if (!preg_match("/([0-9]+)/", $_POST["phone"])) {
+				$response["status"] = "error";
+				$response["error_code"] = "incorrect_fields";
+				$response["fields_names"][] = "phone";
+			} else {
+				CModule::IncludeModule("iblock");
+				$el = new CIBlockElement;
+				$arFields = array(
+					"IBLOCK_ID" => 22,
+					"ACTIVE" => "N",
+					"DATE_ACTIVE_FROM" => date("d.m.Y H:i:s"),
+					"NAME" => "-",
+					"PROPERTY_VALUES" => array(
+						"lang" => $_POST["lang"],
+						"name" => $_POST["name"],
+						"last_name" => $_POST["last_name"],
+						"phone" => $_POST["phone"],
+						"in_date" => $_POST["in_date"],
+						"out_date" => $_POST["out_date"],
+						"house_name" => $_POST["house_name"],
+						"house_id" => $_POST["house_id"],
+						"people_count" => $_POST["people_count"],
+						"people_count_2" => $_POST["hidden_people_count"],
+					)
+				);
+
+				$add = $el->Add($arFields);
+
+				if ($add) {
+					$response["status"] = "success";
+
+					$site_id = $_POST["lang"] == "ru" ? "s1" : "en";
+					//$send = CEvent::SendImmediate(
+						//"QUESTION_SENT",
+						//$site_id,
+						//false,
+						//"N"
+					//);
+				} else {
+					$response["status"] = "error";
+					$response["error_code"] = "add_to_iblock";
 				}
 			}
 		}
