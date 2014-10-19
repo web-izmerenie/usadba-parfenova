3ddb749a1192095d2199ea726c20da4b896e12ac
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-19 06:49:03 +0400

    update in structure of "workshop" section

--- bitrix/templates/.default/components/bitrix/news.detail/workshop.detail/template.php
+++ bitrix/templates/.default/components/bitrix/news.detail/workshop.detail/template.php
@@ -1,24 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <section class="workshop">
-	<?if($arResult["DETAIL_PICTURE"]){?>
-		<div class="panorama" data-texture="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"></div>
-	<?}?>
-	<?if($arResult["DISPLAY_PROPERTIES"]["ALT_PHOTO"]["VALUE"]){?>
-		<?$thumb = CFile::ResizeImageGet(
-			$arResult["DISPLAY_PROPERTIES"]["ALT_PHOTO"]["VALUE"],
-			array(
-				"width" => "1200",
-				"height" => "1200"
-			),
-			BX_RESIZE_IMAGE_PROPORTIONAL
-		);?>
-		<div class="detail_picture"><img alt="" src="<?=$thumb["src"]?>" /></div>
-	<?}?>
-	<div class="notation_block">
-		<?=$arResult["DETAIL_TEXT"]?>
-	</div>
-
 	<div class="photogallery">
 		<div class="new_gallery_big">
 			<a class="prev"></a>
@@ -32,5 +14,9 @@
 				<li><a href="<?=$origin["src"]?>" target="_blank"><img alt="" src="<?=$thumb["src"]?>" /></a></li>
 			<?}?>
 		</ul>
+	</div>
+
+	<div class="notation_block">
+		<?=$arResult["DETAIL_TEXT"]?>
 	</div>
 </section>
